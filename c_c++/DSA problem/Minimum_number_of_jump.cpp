6d430eaf2ec2b2d76967c0e0a291ebf8828aab76
rakibul23n28 <hasa23n28@gmail.com>
2023-07-29 23:00:44 +0600

    first commit

---
+++ c_c++/DSA problem/Minimum_number_of_jump.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+                int count = 0;
+        int pos=0;
+        int maxi=0;
+        for(int i=0; i<n; i++){
+            maxi = max(maxi,i+arr[i]);
+            if(i==pos){
+                pos = maxi;
+                count++;
+            }
+            if(pos>=n-1)
+            return count;
+            
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
