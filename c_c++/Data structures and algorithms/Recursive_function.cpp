0a69d8c8941a108d2f10e063389434c08716dd03
rakibul23n28 <hasa23n28@gmail.com>
2023-08-03 19:42:42 +0600

    first commit

--- c_c++/Data structures and algorithms/Recursive_function.cpp
+++ c_c++/Data structures and algorithms/Recursive_function.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 
+
 int count_total(int size_of_string,std::string s){
     if  (s[size_of_string] != '\0') return 1+count_total(++size_of_string,s);
     else return 0;
