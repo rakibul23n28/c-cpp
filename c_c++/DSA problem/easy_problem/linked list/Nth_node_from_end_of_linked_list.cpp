c10db6f81db14044ebdf6cc823a4c2a2da2c6824
rakibul23n28 <hasa23n28@gmail.com>
2023-08-16 10:57:11 +0600

    first commit

---
+++ c_c++/DSA problem/easy_problem/linked list/Nth_node_from_end_of_linked_list.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for C++
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+    int getNthFromLast(Node *head, int n)
+    {
+           Node* remp=head;
+           int x=0;
+           while(remp!=NULL){
+               x++;
+               remp=remp->next;
+           }
+           if(n>x) return -1;
+           remp=head;
+           int i=0;
+           while(remp!=NULL){
+               i++;
+               if(i==(x-n+1)) return remp->data;
+               remp=remp->next;
+           }
+           return -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
